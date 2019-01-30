7a6266c5537ac02fda6112813ef52f785bfc233d
Osman Kanadikirik <ookndkrk@gmail.com>
2019-01-30 20:19:14 +0300

    added timer available feature

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Switch } from 'react-native';
 import { Accelerometer } from 'expo';
 
 export default class App extends React.Component {
   state = {
     accelerometerData: {},
     second: 0,
-    timerActive: false,
+    isTimerActive: false,
+    isTimerAvailable: true,
   }
 
   componentDidMount() {
@@ -19,13 +20,13 @@ export default class App extends React.Component {
 
   _subscribe = () => {
     this._subscription = Accelerometer.addListener(accelerometerData => {
-      this._handleTimer(accelerometerData.z);
+      this._handleUpright(accelerometerData.z);
     });
   }
 
-  _handleTimer = (z) => {
-    if(z > 0.9){
-      if(!this.state.timerActive)
+  _handleUpright = (z) => {
+    if(z > 0.9 || z < -0.9){
+      if(!this.state.isTimerActive)
         this._startTimer();
     } else 
         this._stopTimer();
@@ -35,15 +36,16 @@ export default class App extends React.Component {
     this.timer = setInterval(() => {
       this.setState({ second: this.state.second+1 })
     },1000)
-    this.setState({ timerActive: true });
+    this.setState({ isTimerActive: true });
   }
 
   _stopTimer = () => {
     clearInterval(this.timer);
-    this.setState({ timerActive: false });
+    this.setState({ isTimerActive: false });
   }
 
   _unsubscribe = () => {
+    this._stopTimer();
     this._subscription && this._subscription.remove();
     this._subscription = null;
   }
@@ -56,20 +58,35 @@ export default class App extends React.Component {
       return `${Math.floor(second/60)} minutes ${second%60} seconds`
   }
 
+  _handleTimerAvailable = () => {
+    let { isTimerAvailable } = this.state;
+    isTimerAvailable ? this._unsubscribe() : this._subscribe();
+    this.setState({ isTimerAvailable: !isTimerAvailable });
+  }
+
   render() {
-    let { timerActive } = this.state;
+    let { isTimerAvailable, isTimerActive } = this.state;
+
+    if(isTimerAvailable){
+      if(isTimerActive)
+        this.informationMessage = "Keep the phone upright for stop the timer"
+      else 
+        this.informationMessage = "Put the phone to the straight surface for start the timer"
+    } else {
+      this.informationMessage = "Toggle the switch for make timer available"
+    }
+
     return (
       <View style={styles.container}>
         <StatusBar hidden />
-        <Text style={styles.headText}>Auto Timer</Text>
+        <View style={styles.header}>
+          <Text style={styles.headText}>Auto Timer</Text>
+          <Switch value={isTimerAvailable} onValueChange={this._handleTimerAvailable} style={styles.switch} />
+        </View>
         <View style={styles.timeContainer}>
           <Text style={styles.timeText}>{this._configureTime()}</Text>
           <Text>
-            {timerActive ? 
-              "Keep the phone upright for stop the timer"
-            :
-              "Put the phone to the straight surface for start the timer"
-            }
+            {this.informationMessage}
           </Text>
         </View>
         <Text>Created by ook0</Text>
@@ -85,6 +102,11 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     paddingVertical: 20
   },
+  header: {
+    flexDirection: 'row',
+    alignItems:'center',
+    justifyContent: 'space-between'
+  },
   timeContainer:{
     textAlign: 'center',
   },
@@ -96,5 +118,8 @@ const styles = StyleSheet.create({
   timeText:{
     fontSize: 22,
     textAlign: 'center'
+  },
+  switch:{
+    transform: [{scale: 1.3}]
   }
 });
